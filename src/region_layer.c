7fc0f3b2dd6c1bb1311bae2c88386cd734067a05
Kamyar <kammirzazad@gmail.com>
2021-05-09 18:58:52 -0500

    updated region_cost::thresh!=0

--- src/region_layer.c
+++ src/region_layer.c
@@ -217,7 +217,7 @@ void  adjustRegionLossesDREML(const region_layer l, network_state state, int ind
     {
         if(l.output[index + 4] > state.dreml_det_thresh)
         {
-            l.delta[index + 4] = l.object_scale * l.output[index + 4] * logistic_gradient(l.output[index + 4]);
+            l.delta[index + 4] = l.object_scale * (1.0 - l.output[index + 4]) * logistic_gradient(l.output[index + 4]);
 
             for(coord_id = 0; coord_id < l.coords; coord_id++)
             {
@@ -239,17 +239,17 @@ void  adjustRegionLossesDREML(const region_layer l, network_state state, int ind
                 // softmax gradient is itself
                 if(prob > state.dreml_det_thresh)
                 {
-                    l.delta[index2] = l.class_scale * l.output[index2];
+                    l.delta[index2] = l.class_scale * (1.0 - l.output[index2]);
                 }
                 else
                 {
-                    l.delta[index2] = l.class_scale * (l.output[index2] - 1.0);
+                    l.delta[index2] = l.class_scale * (0.0 - l.output[index2]);
                 }
             }
         }
         else
         {
-            l.delta[index + 4] = l.noobject_scale * (l.output[index + 4] - 1.0) * logistic_gradient(l.output[index + 4]);
+            l.delta[index + 4] = l.noobject_scale * (0.0 - l.output[index + 4]) * logistic_gradient(l.output[index + 4]);
 
             for(coord_id = 0; coord_id < l.coords; coord_id++)
             {
