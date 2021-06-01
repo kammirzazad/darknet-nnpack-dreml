2bd65c097d9ee0e471d5db3bffb16c9bb4566cd2
Kamyar <kammirzazad@gmail.com>
2021-06-01 12:19:39 -0500

    only objectness is non-zero

--- src/region_layer.c
+++ src/region_layer.c
@@ -248,7 +248,7 @@ void  adjustRegionLossesDREML(const region_layer l, network_state state, int ind
 
             for(coord_id = 0; coord_id < l.coords; coord_id++)
             {
-                l.delta[index + coord_id] = l.anchor_boxes[n] * l.coord_scale * (1.0 - objectness);
+                l.delta[index + coord_id] = l.anchor_boxes[n] * l.coord_scale * (1.0 - objectness) * 0.0;
 
                 // only first two coordinates go through logistic
                 if(coord_id < 2)
@@ -266,11 +266,11 @@ void  adjustRegionLossesDREML(const region_layer l, network_state state, int ind
                 // softmax gradient is itself
                 if(prob > state.dreml_det_thresh)
                 {
-                    l.delta[index2] = l.anchor_boxes[n] * l.class_counts[class_id] * l.class_scale * (1.0 - objectness) * (1.0 - l.output[index2]);
+                    l.delta[index2] = l.anchor_boxes[n] * l.class_counts[class_id] * l.class_scale * (1.0 - objectness) * (1.0 - l.output[index2]) * 0.0;
                 }
                 else
                 {
-                    l.delta[index2] = l.anchor_boxes[n] * l.class_counts[class_id] * l.class_scale * (1.0 - objectness) * (0.0 - l.output[index2]);
+                    l.delta[index2] = l.anchor_boxes[n] * l.class_counts[class_id] * l.class_scale * (1.0 - objectness) * (0.0 - l.output[index2]) * 0.0;
                 }
             }
        }
